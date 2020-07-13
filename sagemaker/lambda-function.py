0bcfa14a015831bae3230982153697895c961178
Samiron Paul <samironpaul@gmail.com>
2020-07-13 10:13:57 -0300

    removed invalid endpoint name

--- sagemaker/lambda-function.py
+++ sagemaker/lambda-function.py
@@ -10,7 +10,7 @@ def lambda_handler(event, context):
     runtime = boto3.Session().client('sagemaker-runtime')
 
     # Now we use the SageMaker runtime to invoke our endpoint, sending the review we were given
-    response = runtime.invoke_endpoint(EndpointName = 'sagemaker-pytorch-2020-07-13-04-28-09-158',    # The name of the endpoint we created
+    response = runtime.invoke_endpoint(EndpointName = '',    # The name of the endpoint we created
                                        ContentType = 'text/plain',                 # The data format that is expected
                                        Body = event['body'])                       # The actual review
 
